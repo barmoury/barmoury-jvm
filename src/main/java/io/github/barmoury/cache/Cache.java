3dafb443cb3d57cc6fe7f55d7bee34d453d68cb4
Thecarisma <iamthecarisma@gmail.com>
2024-05-07 10:20:05 +0100

    update the cache max buffer size

--- src/main/java/io/github/barmoury/cache/Cache.java
+++ src/main/java/io/github/barmoury/cache/Cache.java
@@ -6,7 +6,7 @@ public interface Cache<T> {
     void cache(T data);
 
     default long maxBufferSize() {
-        return 4; //150;
+        return 150;
     }
 
     default long intervalBeforeFlush() {
